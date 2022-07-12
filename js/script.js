3baf4805f940299b2ade9f0d62f7106f5058f8b0
Gabriela Chesim <gabycsouza@gmail.com>
2022-07-12 18:41:28 -0300

    Adiciona código do projeto final

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(
+    function(){
+        $('.js--nav-icon').click(
+            function(){
+                var nav = $('.main-nav');
+
+                nav.slideToggle(200);
+            });          
+});
+
